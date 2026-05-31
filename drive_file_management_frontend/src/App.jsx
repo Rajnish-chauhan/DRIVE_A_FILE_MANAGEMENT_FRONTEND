49b386cb83df1788e51afc5a900c2ef73feb816c
Rajnish Chauhan <crajnish425@gmail.com>
2026-06-01 00:53:12 +0530

    set default package.json -Build

--- drive_file_management_frontend/src/App.jsx
+++ drive_file_management_frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DriveApp from "./DriveApp";
 import SimpleLoginPage from "./SimpleLoginPage";
-import Footer from "./Components/Footer";
+import Footer from "./Components/Sidebar";
 
 // VERY IMPORTANT: API calls mein cookies send ke liye
 axios.defaults.withCredentials = true;
